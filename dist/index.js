024385085e26d373c60a26249bd9b18d2ad9e407
Allan Raposo <Araposo1@JHU.EDU>
2022-10-16 21:19:36 -0400

    Updated the build

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
-'use strict';
-console.log('hello world!');
+"use strict";
+const core = require('@actions/core');
+const github = require('@actions/github');
+async function run() {
+    // Generate the OIDC token
+    const oidcToken = await core.getIDToken();
+    core.setOutput('token', oidcToken);
+}
+run();
