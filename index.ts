34b00601b4222e254dd99263b66b6666a93bcb58
Allan Raposo <Araposo1@JHU.EDU>
2022-10-21 16:41:41 -0400

    testing the clone and rename

--- index.ts
+++ index.ts
@@ -69,7 +69,7 @@ async function run(): Promise<void> {
   cloneRepo(repoUrl);
   moveFile(path.join(topUrl, 'csf-hw3'), path.join(topUrl, 'source'));
   //moveFile(path.join(topUrl, 'csf-hw3'), path.join(topUrl, 'submission'));
-  console.info(github.event.repository.name);
+  console.info(JSON.stringify(github.context.payload, undefined, 2));
   console.info('\n ');
   fs.readdirSync(build_path()).forEach((file: any) => {
     console.info(file);
