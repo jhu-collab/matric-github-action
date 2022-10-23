ab14c80ebf33479f7b1f3755b36ba3951395cb97
Allan Raposo <Araposo1@JHU.EDU>
2022-10-23 15:14:43 -0400

    misisng to validate the json

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import {
   PayloadRepository,
   WebhookPayload,
 } from '@actions/github/lib/interfaces';
+import { resolve } from 'path';
 
 require('dotenv').config();
 const core = require('@actions/core');
@@ -47,35 +48,86 @@ async function genRepoUrl(assignmentId: string, courseId: string) {
   }
 }
 
-async function cloneRepo(repoName: string, url: string): Promise<void> {
+async function cloneRepo(repoName: string, url: string): Promise<boolean> {
   const directory = build_path();
-  exec(
-    `git clone ${url}`,
-    {
-      cwd: directory,
-    },
-    (error: any, _stdout: any, _stderr: any) => {
-      modifyFile(
-        'mv',
-        path.join(directory, repoName),
-        path.join(directory, 'source'),
+  return new Promise((resolve, reject) => {
+    exec(
+      `git clone ${url}`,
+      {
+        cwd: directory,
+      },
+      async (error: any, _stdout: any, _stderr: any) => {
+        const renamedSource = await modifyFile(
+          'mv',
+          path.join(directory, repoName),
+          path.join(directory, 'source'),
+        );
+
+        if (!renamedSource) {
+          reject('failed to clone and rename repo!');
+        }
+
+        const copiedSetup = await modifyFile(
+          'cp',
+          path.join(directory, 'source', 'setup.sh'),
+          path.join(directory, 'setup.sh'),
+        );
+
+        if (!copiedSetup) {
+          reject('failed to copy the setup.sh file');
+        }
+
+        const copiedRunAutograder = await modifyFile(
+          'cp',
+          path.join(directory, 'source', 'run_autograder'),
+          path.join(directory, 'run_autograder'),
+        );
+
+        if (!copiedRunAutograder) {
+          reject('failed to copy the run_autograder file');
+        }
+        resolve(true);
+      },
+    );
+  });
+}
+
+async function executeSetupAndAutograder(): Promise<boolean> {
+  const dir = build_path();
+  return new Promise(async (resolve, reject) => {
+    // Execute the setup.sh file if it exists and redirect output
+    const setup = await executeFile(
+      path.join(dir, 'setup.sh'),
+      path.join(dir, 'setup.logs.txt'),
+    );
+
+    let autograder = false;
+
+    if (setup) {
+      autograder = await executeFile(
+        path.join(dir, 'run_autograder'),
+        path.join(dir, 'run_autograder.logs.txt'),
       );
-    },
-  );
+    }
+
+    if (!autograder) {
+      reject('failed to run autograder!');
+    }
+
+    resolve(setup && autograder);
+  });
 }
 
 async function sendResults(
   path: string,
-  githubPayload: WebhookPayload,
+  actor: string,
+  commitId: string,
+  repoName: string,
 ): Promise<void> {
   try {
     const resultsJSON = readJSONFile(path);
-    //const commitId =
-    //const actor = githubPayload.
-    const repoName = githubPayload?.repository?.name;
-    const payload = {};
-    const res = await axios.get(`${process.env.SERVER_HOST}/autograders/`);
-    return res.data;
+    const payload = { repoName, actor, commitId, results: resultsJSON };
+    await axios.post(`${process.env.SERVER_HOST}/submission/`, payload);
   } catch (error) {
     console.error(error);
   }
@@ -91,48 +143,42 @@ function validateResults(path: string): boolean {
 async function run(): Promise<void> {
   const dir = build_path();
   const payload = github.context.payload;
-  core.info(payload);
-  core.info('\n');
-  core.info(github.context);
-  console.log(payload);
-  console.log(github.context);
-  const repoName = 'hsf'; //payload.repository.name;
-  //const oidcToken = await core.getIDToken();
-  //const { courseId, assignmentId } = await genMatricTokenInfo(oidcToken);
-  const repoUrl = 'https://github.com/ARaps1/csf-hw3.git';
-  //const repoUrl = await genRepoUrl(courseId, assignmentId);
-  //cloneRepo('csf-hw3', repoUrl);
-  //modifyFile('cp', path.join(dir, repoName), path.join(dir, 'submission'));
-  //createFolder(path.join(dir, 'results'));
+  const repoName = payload.repository.name;
+  const commitId = payload.head_commit.id;
+  const actor = payload.head_commit.committer.username;
 
-  //copy the setup.sh file
+  const oidcToken = await core.getIDToken();
+  const { courseId, assignmentId } = await genMatricTokenInfo(oidcToken);
+  const repoUrl = await genRepoUrl(courseId, assignmentId);
+  const repoClonedAndRenamed = await cloneRepo('csf-hw3', repoUrl);
 
-  /*modifyFile(
+  await modifyFile(
     'cp',
-    path.join(dir, 'source', 'setup.sh'),
-    path.join(dir, 'setup.sh'),
+    path.join(dir, repoName),
+    path.join(dir, 'submission'),
   );
-  */
 
-  // Execute the setup.sh file if it exists and redirect output
-  //executeFile(path.join(dir, 'script.sh'), path.join(dir, 'setup.logs.txt'));
+  createFolder(path.join(dir, 'results'));
 
-  // copy the run_autograder file
-  /* modifyFile(
-    'cp',
-    path.join(dir, 'source', 'setup.sh'),
-    path.join(dir, 'setup.sh'),
-  );
-  
-  //Execute the run_autograder file
-  executeFile(
-    path.join(dir, 'run_autograder'),
-    path.join(dir, 'run_autograder.logs.txt'),
-  );
-  */
+  if (!repoClonedAndRenamed) {
+    core.error('Failed to clone the autograder repo!');
+    return;
+  }
+
+  const setupAndAutograder = await executeSetupAndAutograder();
 
   // Verify that there is a file in results/results.json
-  //console.log(validateResults(path.join(dir, 'results/results.json')));
+  if (
+    setupAndAutograder &&
+    validateResults(path.join(dir, 'results/results.json'))
+  ) {
+    sendResults(
+      path.join(dir, 'results/results.json'),
+      actor,
+      commitId,
+      repoName,
+    );
+  }
 }
 
 run();
--- src/util/file-util.ts
+++ src/util/file-util.ts
@@ -6,29 +6,42 @@ async function modifyFile(
   cmd: string,
   oldPath: string,
   newPath: string,
-): Promise<void> {
+): Promise<boolean> {
   if (!fileExists(oldPath)) {
     console.log(`${oldPath} does not exists!`);
-    return;
+    return false;
   }
 
-  exec(
-    `${cmd} ${oldPath} ${newPath}`,
-    (_error: any, _stdout: any, _stderr: any) => {},
-  );
+  return new Promise((resolve, reject) => {
+    exec(
+      `${cmd} ${oldPath} ${newPath}`,
+      (error: any, _stdout: any, _stderr: any) => {
+        if (error != null) {
+          console.warn(error);
+          reject(`Failed to ${cmd} files!`);
+        }
+        resolve(true);
+      },
+    );
+  });
 }
 
-async function writeOutputToFile(stdout: string, outputPath: string) {
-  fs.open(outputPath, 'w', (error: any, fd: any) => {
-    if (error != null) {
-      console.error(`error: ${error}`);
-      return;
-    }
-
-    fs.write(fd, stdout, (error: Error) => {
+async function writeOutputToFile(
+  stdout: string,
+  outputPath: string,
+): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    fs.open(outputPath, 'w', (error: any, fd: any) => {
       if (error != null) {
-        console.error(`error: ${error}`);
+        reject(`error: ${error}`);
       }
+
+      fs.write(fd, stdout, (error: Error) => {
+        if (error != null) {
+          reject(`error: ${error}`);
+        }
+        resolve(true);
+      });
     });
   });
 }
@@ -36,24 +49,20 @@ async function writeOutputToFile(stdout: string, outputPath: string) {
 async function executeFile(
   filePath: string,
   outputFilePath: string,
-): Promise<void> {
+): Promise<boolean> {
   if (!fileExists(filePath)) {
     console.log('file does not exists!');
-    return;
+    return false;
   }
-
-  execFile(filePath, (error: any, stdout: any, stderr: any) => {
-    if (error != null) {
-      console.error(`error: ${error}`);
-      return;
-    }
-
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-
-    writeOutputToFile(stdout, outputFilePath);
+  return new Promise((resolve, reject) => {
+    execFile(filePath, async (error: any, stdout: any, _stderr: any) => {
+      if (error != null) {
+        reject(`error: ${error}`);
+        return;
+      }
+      const wroteSuccessfully = await writeOutputToFile(stdout, outputFilePath);
+      resolve(wroteSuccessfully);
+    });
   });
 }
 
